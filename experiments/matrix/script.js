c893e11134e690be2617cd2e0514f2c8d2dedbf2
Vladnik2k <vlad.nikitiuk@useblanco.com>
2021-08-29 15:59:45 +0300

    Fix matrix optimization Refactor

--- experiments/matrix/script.js
+++ experiments/matrix/script.js
@@ -1,8 +1,34 @@
-let symbols = [];
+let allSymbols = [];
+let symbolsToShow = [];
 
 function generate() {
-    for (let i = 0; i < numberOfSymbolsInScreen; i++) {
-        symbols.push(new Symbol(randomYPosition()));
+    for (let i = 0; i < maxNumberOfSymbols; i++) {
+        allSymbols.push(new Symbol(randomYPosition()));
+    }
+
+    symbolsToShow = allSymbols.slice(0, numberOfSymbolsInScreen);
+}
+
+function getMapWithColors() {
+    const map = new Map();
+    for (let i = 0; i < symbolsToShow.length; i++) {
+        const array = map.get(symbolsToShow[i].color) || [];
+        array.push(symbolsToShow[i]);
+        map.set(symbolsToShow[i].color, array);
+    }
+
+    return map;
+}
+
+function showSymbolsPerColor(color, symbols) {
+    ctx.fillStyle = color;
+    for (let i = 0; i < symbols.length; i++) {
+        if (symbols[i].y > canvas.height) {
+            symbols[i].generateStartValues();
+        }
+
+        symbols[i].move();
+        ctx.fillText(symbols[i].symbol, symbols[i].numberOfColumn * barWidth, symbols[i].y);
     }
 }
 
@@ -10,14 +36,10 @@ function animation() {
     ctx.fillStyle = backgroundColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    symbols.forEach(symbol => {
-        if (symbol.y > canvas.height) {
-            symbol.generateStartValues();
-        }
+    const map = getMapWithColors();
 
-        symbol.move();
-        symbol.draw();
-    });
+    ctx.font = `${barWidth}px serif`;
+    Array.from(map.entries()).forEach(entry => showSymbolsPerColor(entry[0], entry[1]));
 
     stats.update();
     requestAnimationFrame(animation);
--- experiments/matrix/symbol.js
+++ experiments/matrix/symbol.js
@@ -14,13 +14,7 @@ class Symbol {
         this.numberOfColumn = Math.floor(Math.random() * (canvas.width / barWidth));
         this.speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
         this.color = symbolColor(this.speed);
-        this.symbol = allSymbols[Math.floor(Math.random() * allSymbols.length)];
-    }
-
-    draw() {
-        ctx.font = `${barWidth}px serif`;
-        ctx.fillStyle = this.color;
-        ctx.fillText(this.symbol, this.numberOfColumn * barWidth, this.y);
+        this.symbol = allowedSymbols[Math.floor(Math.random() * allowedSymbols.length)];
     }
 
     move() {
